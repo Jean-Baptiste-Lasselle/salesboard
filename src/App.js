52eddfb3ec92c81da20a6784abe922f9a5a40adc
Winfred Selwyn Ooh Azlin <winfredselwyn@gmail.com>
2016-11-24 14:40:58 +0800

    stashing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import { HorizonRoute, connect } from 'react-hz'
 import Main from './main/Main'
+import Horizon from '@horizon/client'
 
 class App extends Component {
 	render() {
 		return (
 			<div>
 				<Main />
+				{this.props.openIssues}
 			</div>
 		)
 	}
--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
 import * as V from 'victory'
+import Horizon from '@horizon/client'
 
+
+const horizon = Horizon({host: 'localhost:8181'})
+// const Horizon = window.Horizon
+// const horizonInstance = Horizon()
 
 ReactDOM.render(
   <App />,
