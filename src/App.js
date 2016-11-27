6451dc522d2b235cc52f5db78c44d41d4fe93305
winfredselwyn <winfredselwyn@gmail.com>
2016-11-27 04:56:34 +0000

    added react-router

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
 		return (
 			<div>
 				<Main />
+				{this.props.children}
 			</div>
 		)
 	}
--- src/index.js
+++ src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
 import * as V from 'victory'
-import { Desklamp, Container } from 'desklamp'
+import { Router, Route, IndexRoute, IndexLink, Link, browserHistory } from 'react-router'
 import Main from './main/Main'
 import EmployeeLocations from './main/EmployeeLocations'
 import AllIssues from './main/AllIssues'
 
+// <Router>
+//   <Route path="/" component={Main}>
+//     <IndexRoute component={App}/>
+//   </Route>
+// </Router>
+// const { Router, Route, IndexRoute, IndexLink, Link } = ReactRouter
+
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute path="main" component={App}/>
+      <Route path="locations" component={EmployeeLocations}/>
+      <Route path="issues" component={AllIssues}/>
+    </Route>
+  </Router>,
   document.getElementById('root')
 )
